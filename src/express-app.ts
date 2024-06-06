9c08d59c14c21a6112e2275864aa3b8e3eea97c4
TakaRuangs <104978628+TakaRuangshawk@users.noreply.github.com>
2024-06-06 18:54:57 +0700

    add login&add design 20240606

--- src/express-app.ts
+++ src/express-app.ts
@@ -7,18 +7,18 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import http from "http";
 import createError from "http-errors";
-import { expressPort,logDrive } from "../package.json";
+import { expressPort, logDrive } from "../package.json";
 import fs from "fs";
 import { Request, Response } from 'express';
 
 const app = express();
 const router = express.Router();
 
-const logDirectory = logDrive +"://logs";
+const logDirectory = logDrive + "://logs";
 
 function writeLogToFile(logMessage: string) {
   const currentDate = new Date();
-  const dateString = currentDate.toISOString().slice(0,10).replace(/-/g,"");
+  const dateString = currentDate.toISOString().slice(0, 10).replace(/-/g, "");
   const logFilePath = path.join(logDirectory, `MTLog_${dateString}.log`);
 
   if (!fs.existsSync(logDirectory)) {
@@ -32,27 +32,58 @@ function writeLogToFile(logMessage: string) {
   });
 }
 
-const routes = [
-  { path: "/", viewName: "index", title: "Home" },
-  { path: "/pageTwo", viewName: "pageTwo", title: "Page 2" },
-  { path: "/pageThree", viewName: "pageThree", title: "Page 3" },
-  { path: "/pageFour", viewName: "pageFour", title: "Page 4" },
-  { path: "/sendMessage", method: "post", handler: sendMessageHandler }
+const filePath = path.join(__dirname, 'users.json');
+const defaultUsers = [
+  { username: 'administrator', password: '111111' },
+  { username: 'bankoperation', password: '111111' },
 ];
 
-routes.forEach(({ path, viewName, title, method, handler }) => {
-  if (method === "post") {
-    router.post(path, handler);
-  } else {
-    router.get(path, (_req, res) => {
-      if (viewName && title) {
-        res.render(viewName, { title });
-      } else {
-        res.status(500).send("Internal Server Error");
-      }
-    });
+if (!fs.existsSync(filePath)) {
+  fs.writeFileSync(filePath, JSON.stringify(defaultUsers, null, 2));
+}
+
+// Function to read users from the JSON file
+const readUsers = (): { username: string, password: string }[] => {
+  if (fs.existsSync(filePath)) {
+    const data = fs.readFileSync(filePath, 'utf8');
+    return JSON.parse(data);
   }
-});
+  return defaultUsers;
+};
+
+// Function to write users to the JSON file
+const writeUsers = (users: { username: string, password: string }[]): void => {
+  fs.writeFileSync(filePath, JSON.stringify(users, null, 2));
+};
+
+const loginHandler = (req: Request, res: Response) => {
+  console.log('POST /login', req.body);
+  const { username, password } = req.body;
+  const users = readUsers();
+  const user = users.find(u => u.username === username && u.password === password);
+
+  if (user) {
+    res.json({ success: true, redirect: `/index?username=${username}` });
+  } else {
+    res.status(401).json({ error: 'Please enter correct password' });
+  }
+};
+
+const changePasswordHandler = (req: Request, res: Response) => {
+  console.log('POST /change-password', req.body);
+  const { username, oldPassword, newPassword } = req.body;
+  let users = readUsers();
+  const userIndex = users.findIndex(u => u.username === username && u.password === oldPassword);
+
+  if (userIndex !== -1) {
+    users[userIndex].password = newPassword;
+    writeUsers(users);
+    res.json({ message: 'Password changed successfully' });
+  } else {
+    res.status(400).json({ error: 'Please enter correct password' });
+  }
+};
+
 function formatDate(date: Date): string {
   const pad = (num: number, size: number): string => {
     let s = "000" + num;
@@ -69,6 +100,7 @@ function formatDate(date: Date): string {
 
   return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}:${milliseconds}`;
 }
+
 function sendMessageHandler(req: Request, res: Response) {
   const message = req.body.message;
   const logMessage = `[${formatDate(new Date())}] ${message}`;
@@ -77,12 +109,43 @@ function sendMessageHandler(req: Request, res: Response) {
   res.json({ success: true, message: "Message received and logged" });
 }
 
+const routes = [
+  { path: "/", viewName: "login", title: "Login" },
+  { path: "/index", viewName: "index", title: "Home" },
+  { path: "/pageTwo", viewName: "pageTwo", title: "Page 2" },
+  { path: "/pageThree", viewName: "pageThree", title: "Page 3" },
+  { path: "/pageFour", viewName: "pageFour", title: "Page 4" },
+  { path: "/sendMessage", method: "post", handler: sendMessageHandler },
+  { path: "/login", method: "post", handler: loginHandler },
+  { path: "/change-password", method: "post", handler: changePasswordHandler }
+];
+
+routes.forEach(({ path, viewName, title, method, handler }) => {
+  if (method === "post") {
+    router.post(path, handler);
+  } else {
+    router.get(path, (req, res) => {
+      if (viewName && title) {
+        const { username } = req.query;
+        if (viewName === "index") {
+          res.render(viewName, { title, username });
+          console.log('username : '+ username);
+        } else {
+          res.render(viewName, { title });
+          console.log('no user required');
+        }
+      } else {
+        res.status(500).send("Internal Server Error");
+      }
+    });
+  }
+});
+
 app.set("port", expressPort);
 app.set("views", path.join(__dirname, "..", "views"));
 app.set("view engine", "ejs");
 
-//app.use(cors()); // Enable CORS
-
+app.use(cors()); // Enable CORS
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -105,6 +168,7 @@ app.use(cors({
   credentials: true,
   allowedHeaders: ['Content-Type', 'Authorization', 'Apikey']
 }));
+
 // CORS middleware for your Electron app's server running on port 3000
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:3000');
@@ -120,6 +184,12 @@ app.options('/lpk/api/v1/kiosk/banknote', (req, res) => {
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, Apikey'); // Add 'Apikey' to the list of allowed headers
   res.status(200).end();
 });
+
+// Catch-all route for handling 404 errors
+app.use((req, res) => {
+  res.status(404).json({ error: 'Not Found' });
+});
+
 const server = http.createServer(app);
 
 function handleServerError(error: any) {
